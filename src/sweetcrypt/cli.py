945610e8d9ab5c9e597be1b251bb3d338563bf07
vnvarma39 <vnvarma39@gmail.com>
2025-04-06 09:42:04 +0530

    Update cli.py

--- src/sweetcrypt/cli.py
+++ src/sweetcrypt/cli.py
@@ -2,10 +2,15 @@ import argparse
 from .core import GlycanCrypt
 
 def main():
-    parser = argparse.ArgumentParser(description="SweetCrypt Glycan Encryptor")
-    # Add CLI arguments here
+    parser = argparse.ArgumentParser(description="SweetCrypt CLI")
+    parser.add_argument("--encrypt", help="Text to encrypt")
+    parser.add_argument("--decrypt", help="File to decrypt")
     args = parser.parse_args()
-    # Implement CLI functionality
     
-if __name__ == "__main__":
-    main()
+    if args.encrypt:
+        crypto = GlycanCrypt(input("Passphrase: "))
+        print(crypto.encrypt(args.encrypt))
+    
+    elif args.decrypt:
+        crypto = GlycanCrypt(input("Passphrase: "))
+        print(crypto.decrypt(args.decrypt))
